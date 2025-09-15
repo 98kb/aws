0dcca0cd3d997380b963226955090d83932c2f49
Yashodhan Singh <dsfx3d@gmail.com>
2025-09-16 00:18:29 +0530

    refactor: restructure EcrPublisher class for improved context management

--- packages/ecr-publisher/src/createEcrPublisher.ts
+++ packages/ecr-publisher/src/createEcrPublisher.ts
@@ -1,4 +1,3 @@
-/* eslint-disable require-atomic-updates */
 import type {ECRClient} from "@aws-sdk/client-ecr";
 import {ensureECRRepo} from "./ensureECRRepo";
 import {toLatestImageTag} from "./toLatestImageTag";
@@ -10,31 +9,38 @@ import type {Context} from "./Context";
 import type {Hook} from "./Hook";
 
 export function createEcrPublisher(ecr: ECRClient) {
-  let context = {} as Context;
-  const preBuildHooks: Hook[] = [];
-  const applyHooks = async (hooks: Hook[]) => {
-    for await (const hook of hooks) {
-      context = await hook(context);
-    }
-  };
-  return {
-    beforeBuild(fn: Hook) {
-      preBuildHooks.push(fn);
-      return this;
-    },
+  return new EcrPublisher(ecr);
+}
 
-    // eslint-disable-next-line max-statements
-    publish: async (options: PublishEcrOptions) => {
-      const self = this as unknown;
-      context.ecr = ecr;
-      context.options = options;
-      await ensureECRRepo(context);
-      context.currentVersion = (await toLatestImageTag(context)) ?? "0.0.0";
-      context.newVersion = options.versionPrefix + bumpVersion(context);
-      const localImageTag = await buildDockerImage(context, {
-        preBuild: applyHooks.bind(self, preBuildHooks),
-      });
-      await uploadImageToECR(context, localImageTag);
-    },
-  } as const;
+class EcrPublisher {
+  private context: Context = {} as Context;
+
+  private preBuildHooks: Hook[] = [];
+
+  constructor(ecr: ECRClient) {
+    this.context.ecr = ecr;
+  }
+
+  beforeBuild(fn: Hook): EcrPublisher {
+    this.preBuildHooks.push(fn);
+    return this;
+  }
+
+  async publish(options: PublishEcrOptions): Promise<void> {
+    this.context.options = options;
+    await ensureECRRepo(this.context);
+    this.context.currentVersion =
+      (await toLatestImageTag(this.context)) ?? "0.0.0";
+    this.context.newVersion = options.versionPrefix + bumpVersion(this.context);
+    const localImageTag = await buildDockerImage(this.context, {
+      preBuild: this.applyHooks.bind(this, this.preBuildHooks),
+    });
+    await uploadImageToECR(this.context, localImageTag);
+  }
+
+  private async applyHooks(hooks: Hook[]): Promise<void> {
+    for await (const hook of hooks) {
+      this.context = await hook(this.context);
+    }
+  }
 }
